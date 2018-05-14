2b4b4e0c4a431c94a19649771ed86eb88c29347d
Your Name <you@example.com>
2018-05-13 20:35:02 -0500

    f

---
+++ tp.py
@@ -0,0 +1,35 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from random import shuffle
+
+#filename = "facebook_combined.txt"
+filename = "test.txt"
+
+with open(filename) as file:
+    DS = [tuple(line.rstrip('\n').split(' ')) for line in file]
+
+shuffle(DS)
+DSLen = len(DS)
+DSDiv = int(DSLen*0.9)
+G = nx.Graph()
+G.add_edges_from(DS[0:DSDiv])
+GTest = nx.Graph()
+GTest.add_edges_from(DS[DSDiv:DSLen])
+
+#Common Neighbors
+GScore = nx.Graph()
+GTestScore = nx.Graph()
+nodeList = G.nodes()
+for n in nodeList:
+    nonNeighbors = nx.non_neighbors(G,n)
+    for nn in nonNeighbors:
+        scoreCN = len(sorted(nx.common_neighbors(G,n,nn)))
+        if scoreCN > 0:
+            GScore.add_edge(n,nn,score=scoreCN)
+            if (GTest.has_edge(n,nn) or GTest.has_edge(nn,n)) and not GTestScore.has_edge(n,nn) and not GTestScore.has_edge(nn,n) :
+                GTestScore.add_edge(n,nn,score=scoreCN)
+
+print("Precision: ",GTestScore.number_of_edges()/GTest.number_of_edges())
+
+#nx.draw(G, with_labels=True, font_weight='bold')
+#plt.show()
