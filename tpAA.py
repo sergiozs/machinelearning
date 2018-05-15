e40e2235faa7a33ea98dfcee911e7413011205a7
Your Name <you@example.com>
2018-05-15 00:58:58 -0500

    t

--- tpAA.py
+++ tpAA.py
@@ -3,8 +3,8 @@ import networkx as nx
 from random import shuffle
 import time
 
-#filename = "facebook_combined.txt"
-filename = "test.txt"
+filename = "facebook_combined.txt"
+#filename = "test.txt"
 with open(filename) as file:
     DS = [tuple(line.rstrip('\n').split(' ')) for line in file]
 shuffle(DS)
@@ -16,15 +16,11 @@ GTest = nx.Graph()
 GTest.add_edges_from(DS[DSDiv:DSLen])
 
 ##Katz-------------------------------------------------------------------
-print("KATZ")
-start_time = time.time() #BEGIN
-GTestScore = nx.Graph()
-for i in nx.centrality_katz_numpy(G):
-    if i[2] > 0:
-        if GTest.has_edge(i[0],i[1]) or GTest.has_edge(i[1],i[0]):
-            GTestScore.add_edge(i[0],i[1],score=i[2])
-print("Tiempo: ",time.time() - start_time) #END
-print("Precision: ",GTestScore.number_of_edges()/GTest.number_of_edges())
+#print("KATZ")
+#start_time = time.time() #BEGIN
+#code
+#print("Tiempo: ",time.time() - start_time) #END
+#print("Precision: ",GTestScore.number_of_edges()/GTest.number_of_edges())
 
 ##Adamic-Adar------------------------------------------------------------
 #print("ADAMIC-ADAR")
@@ -38,21 +34,21 @@ print("Precision: ",GTestScore.number_of_edges()/GTest.number_of_edges())
 #print("Precision: ",GTestScore.number_of_edges()/GTest.number_of_edges())
 
 ##Common Neighbors--------------------------------------------------------
-#print("COMMON NEIGHBORS")
-#start_time = time.time() #BEGIN
-#GScore = nx.Graph()
-#GTestScore = nx.Graph()
-#nodeList = G.nodes()
-#for n in nodeList:
-#    nonNeighbors = nx.non_neighbors(G,n)
-#    for nn in nonNeighbors:
-#        scoreCN = len(sorted(nx.common_neighbors(G,n,nn)))
-#        if scoreCN > 0:
-#            GScore.add_edge(n,nn,score=scoreCN)
-#            if (GTest.has_edge(n,nn) or GTest.has_edge(nn,n)) and not GTestScore.has_edge(n,nn) and not GTestScore.has_edge(nn,n):
-#                GTestScore.add_edge(n,nn,score=scoreCN)
-#print("Tiempo: ",time.time() - start_time) #END
-#print("Precision: ",GTestScore.number_of_edges()/GTest.number_of_edges())
+print("COMMON NEIGHBORS")
+start_time = time.time() #BEGIN
+GScore = nx.Graph()
+GTestScore = nx.Graph()
+nodeList = G.nodes()
+for n in nodeList:
+    nonNeighbors = nx.non_neighbors(G,n)
+    for nn in nonNeighbors:
+        scoreCN = len(sorted(nx.common_neighbors(G,n,nn)))
+        if scoreCN > 0:
+            GScore.add_edge(n,nn,score=scoreCN)
+            if (GTest.has_edge(n,nn) or GTest.has_edge(nn,n)) and not GTestScore.has_edge(n,nn) and not GTestScore.has_edge(nn,n):
+                GTestScore.add_edge(n,nn,score=scoreCN)
+print("Tiempo: ",time.time() - start_time) #END
+print("Precision: ",GTestScore.number_of_edges()/GTest.number_of_edges())
 
 #nx.draw(G, with_labels=True, font_weight='bold')
 #plt.show()
